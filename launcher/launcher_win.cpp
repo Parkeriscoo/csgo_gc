bfcc028e5edbb3ca243457688f58b149a1addc98
Parker <51893650+Parkeriscoo@users.noreply.github.com>
2025-08-01 18:49:46 -0500

    Update launcher_win.cpp

--- launcher/launcher_win.cpp
+++ launcher/launcher_win.cpp
@@ -154,8 +154,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return 1;
     }
 
-    _snwprintf_s(modulePath, std::size(modulePath), L"%ls\\csgo_gc\\" GC_LIB_DIR "\\"
-                                                    "csgo_gc" GC_LIB_EXTENSION,
+    _snwprintf_s(modulePath, std::size(modulePath), L"%ls\\bin\\" GC_LIB_DIR "\\"
+                                                    "bin" GC_LIB_EXTENSION,
         baseDir);
     InstallGC_t InstallGC = (InstallGC_t)LoadModuleAndFindSymbol(modulePath, "InstallGC");
     if (!InstallGC)
