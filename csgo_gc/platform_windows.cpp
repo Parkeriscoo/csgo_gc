76bbd751c64c31b459f3abc54c9469930e185888
Parker <51893650+Parkeriscoo@users.noreply.github.com>
2025-08-01 18:44:37 -0500

    Update platform_windows.cpp

--- csgo_gc/platform_windows.cpp
+++ csgo_gc/platform_windows.cpp
@@ -34,10 +34,6 @@ void Print(const char *format, ...)
         uint8_t color[4] = { 0, 255, 128, 255 };
         s_ConColorMsg(color, "[GC] %s", buffer);
     }
-
-    FILE *f = fopen("gc_log.txt", "a");
-    fprintf(f, "%s", buffer);
-    fclose(f);
 }
 
 void Error(const char *format, ...)
